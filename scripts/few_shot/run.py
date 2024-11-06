1ce046d2a1a4c1b1e8375bb538f8c74c8954bcbf
bulut <kbozler@email.arizona.edu>
2024-11-06 01:54:11 -0700

    second attempt, getting better

--- scripts/few_shot/run.py
+++ scripts/few_shot/run.py
@@ -10,7 +10,7 @@ API_KEY = "22cff3bda0474158a99c200c58b29267fe4540c8af09fbbfb9499e5741e8031d"  # 
 def create_prompt(df):
     few_shot_prompt = "Following is a series of discharge reports and their summaries. See the patterns in the summaries and generate a similar summary for the last report:\n"
     for i, row in df.iterrows():
-        few_shot_prompt += f"Report: {row['discharge_report']}\nSummary: {row['discharge_summary']}\n\n"
+        few_shot_prompt += f"Report{i+1}:\n {row['discharge_report']}\nSummary{i+1}:\n {row['discharge_summary']}\n -------\n"
     return few_shot_prompt
 
 def generate_summary(text, initial_prompt, model, client):
@@ -18,17 +18,18 @@ def generate_summary(text, initial_prompt, model, client):
     Generates a summary using the Together.AI API
     """
     # Define prompt for summarization
-    prompt = initial_prompt + "Report: " + text + "\nSummary: " 
-    print("generating a summary\n\n")
+    prompt = f"{initial_prompt}Target report:\n {text}\nSummary:\n" 
+    print("generating a summary\n")
     response = client.completions.create(
         model=model,
         prompt=prompt,
+        min_tokens=500,
         max_tokens=2048,  # Adjust based on your expected summary length
-        temperature=0.7,  # Good balance between creativity and consistency
+        temperature=0.65,  # Good balance between creativity and consistency
         top_p=0.9,  # Nucleus sampling to maintain coherence
         top_k=40,  # Limit vocabulary while maintaining flexibility
-        repetition_penalty=1.1,  # Slight penalty to avoid repetitive text
-        stop=["\n\n"],
+        repetition_penalty=1.2,  # Slight penalty to avoid repetitive text
+        stop=["\n -------\n"],
     )
     return response.choices[0].text
 
@@ -39,6 +40,8 @@ test_df = pd.read_csv(os.path.join(data_path, "test.csv"))
 test_df = test_df[5:10]
 few_shot_df = test_df[:2]
 initial_prompt = create_prompt(few_shot_df)
+with open(os.path.join(project_path, "output/few_shot_summaries/initial_prompt.txt"), "w") as f:
+    f.write(initial_prompt)
 
 
 
