b514a907008de775c7f1cd794bd80e15c2ac7c07
kbulutozler <kbulutozler@gmail.com>
2024-11-12 17:18:05 -0700

    finetuning scripts getting cleaned. switched to 2 v100 32gb

--- scripts/finetuning/ft_run.py
+++ scripts/finetuning/ft_run.py
@@ -1,207 +1,176 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, default_data_collator, Trainer, TrainingArguments
+from constants import ZS_SYSTEM_PROMPT, API_KEY, LLM_NAME
+import torch
 import os
 import pandas as pd
-from datasets import Dataset, DatasetDict
-import torch
+import json
+from sklearn.model_selection import train_test_split
+from datasets import Dataset
 from torch.utils.data import DataLoader
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import transformers
-from typing import Any, Dict, List
 import peft
-from huggingface_hub import login 
-login("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")
-
-def load_model_and_tokenizer(model_path):
-    ''' load model and tokenizer '''
-
-    # set quantization configs if using qlora
-    quantization_config = transformers.BitsAndBytesConfig(
-                load_in_4bit=True,
-                bnb_4bit_use_double_quant=True,
-                bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch.bfloat16
-            )
-
-    # define model and tokenizer
-    model = AutoModelForCausalLM.from_pretrained(model_path,
-                                                    quantization_config=quantization_config)
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    
-    if tokenizer.pad_token_id is None: # autoregressive models' pad token not set by default
-        tokenizer.pad_token_id = tokenizer.eos_token_id 
-    
-
-    return model, tokenizer
-
-
-def get_tunable_model(model, device):
-    ''' prep model for param-efficient fine-tuning '''
-
-    task_type = peft.TaskType.CAUSAL_LM
-    
-    # prepare for k-bit training
-    model = peft.prepare_model_for_kbit_training(model) 
-    
-    # get peft configs based on architecture (task_type) and fine-tuning method
-    config = peft.LoraConfig(task_type=task_type, inference_mode=False,
-                                r=8, lora_alpha=32,
-                                lora_dropout=0.1)
-
-    # wrap model w peft configs
-    model = peft.get_peft_model(model, config).to(device)
-    model.print_trainable_parameters()
-
-    return model
-
-
-def preprocess_function(examples, tokenizer, max_length, device):
-    """
-    Preprocess function for autoregressive models using left padding to fit max_length.
-    final version:
-        input_ids: padding + text + label + eos (no masking, full text and label)
-        attention_mask: padding + text + label + eos (all 1s except for padding)
-        labels: padding + label + eos (-100 masks padding and input tokens)
-    """
-     # Insert special tokens into inputs and targets
-    inputs = examples['text']
-    targets = examples['text_label']
-
-
-    # Tokenize inputs and targets without padding
-    model_inputs = tokenizer(inputs, add_special_tokens=False)
-    labels = tokenizer(targets, add_special_tokens=False)
-
-    for i in range(len(inputs)): # for each example input-target pairs
-        # Get tokenized input and label IDs
-        sample_input_ids = model_inputs["input_ids"][i]
-        sample_label_input_ids = labels["input_ids"][i] + [tokenizer.eos_token_id]
-
-        # Append labels to inputs
-        model_inputs["input_ids"][i] = sample_input_ids + sample_label_input_ids
-
-        # Create labels for loss computation (-100 masks input tokens)
-        labels["input_ids"][i] = [-100] * len(sample_input_ids) + sample_label_input_ids
-
-        # Update attention mask
-        model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
-
-    # handle padding from left side, to fit max_length
-    for i in range(len(inputs)):
-        sample_input_ids = model_inputs["input_ids"][i]
-        sample_label_input_ids = labels["input_ids"][i]
-        # pad input ids
-        model_inputs["input_ids"][i] = ([tokenizer.pad_token_id] * 
-                                       (max_length - len(sample_input_ids)) + 
-                                       sample_input_ids)
-        # pad attention mask
-        model_inputs["attention_mask"][i] = ([0] * (max_length - len(sample_input_ids)) +
-                                            model_inputs["attention_mask"][i])
-        # pad labels
-        labels["input_ids"][i] = ([-100] * (max_length - len(sample_label_input_ids)) +
-                                 sample_label_input_ids)
-
-    # Add labels to model inputs
-    model_inputs["labels"] = labels["input_ids"]
-
-    # truncate to max_length, but im not sure if this is necessary since we already calculated max_length on combined input-target length + 1 for eos token
-    model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length],device=device)
-    model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length],device=device)
-    labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length],device=device)
-
-    model_inputs["labels"] = labels["input_ids"]
-
-    return model_inputs
-
-def calculate_max_length(dataset, tokenizer):
-    max_lengths = []
-    
-    # Calculate max length for each split in the dataset
-    tokenized_inputs = [tokenizer(example['text'])['input_ids'] for example in dataset]
-    tokenized_targets = [tokenizer(example['text_label'])['input_ids'] for example in dataset]
-
-    # Get the maximum length from the lengths of each text-text label pair and add 1 for eos token
-    combined_max_length = max([len(input_ids) + len(label_ids) 
-                              for input_ids, label_ids in zip(tokenized_inputs, tokenized_targets)]) + 1
-    return combined_max_length
-
-def get_hf_dataset(data_path):
-    train_df = pd.read_csv(os.path.join(data_path, 'train.csv'))
-    test_df = pd.read_csv(os.path.join(data_path, 'test.csv'))
-    train_df.columns = ['text', 'text_label']
-    test_df.columns = ['text', 'text_label']
-    # inject ||startoftext|| and ||endoftext||
-    train_df['text_label'] = train_df['text_label'].apply(lambda x: "||startoftext||" + x + " ||endoftext||")
-    test_df['text_label'] = test_df['text_label'].apply(lambda x: "||startoftext||" + x + " ||endoftext||")
-    train_df = train_df[:16]
-    test_df = test_df[:8]
-    
-    train_dataset = Dataset.from_pandas(train_df)
-    test_dataset = Dataset.from_pandas(test_df)
-
-    return train_dataset, test_dataset
-
-def get_tokenized_datasets(data_path, tokenizer, device):
-    train_dataset, test_dataset = get_hf_dataset(data_path)
-    train_max_length = calculate_max_length(train_dataset, tokenizer)
-    test_max_length = calculate_max_length(test_dataset, tokenizer)
-
-    train_dataset = train_dataset.map(
-        lambda x: preprocess_function(x, tokenizer=tokenizer, max_length=train_max_length, device=device),
-        batched=True
-    )
-    test_dataset = test_dataset.map(
-        lambda x: preprocess_function(x, tokenizer=tokenizer, max_length=test_max_length, device=device),
-        batched=True
-    )
-
-    return train_dataset, test_dataset
-
-
+from transformers import default_data_collator
+import argparse
+from tqdm import tqdm
+from scripts.finetuning.ft_utils import load_model_and_tokenizer, get_tunable_model, preprocess_data, define_optimizer
 def main():
     
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using device: {device}")
-    torch.cuda.empty_cache()
+    # Check number of GPUs available
+    gpu_count = torch.cuda.device_count()
+    print(f"Number of GPUs available: {gpu_count}")
 
-    # load model and tokenizer with qlora 
-    model_path = "meta-llama/Llama-3.2-1B"
-    model, tokenizer = load_model_and_tokenizer(model_path)
-    model = get_tunable_model(model, device)
-
-    project_path = os.getcwd()
-    data_path = os.path.join(project_path, "data/processed")
-
-    train_dataset, test_dataset = get_tokenized_datasets(data_path, tokenizer, device)
-    # Define training arguments
-    training_args = TrainingArguments(
-        output_dir="./results",
-        learning_rate=1e-3,
-        per_device_train_batch_size=1,
-        num_train_epochs=1,
-        weight_decay=0.01,
-        logging_dir="./logs",
-        logging_steps=2,
-        save_strategy="no",  # Disable intermediate model saving
-        gradient_accumulation_steps=4,  # Set gradient accumulation steps
-        lr_scheduler_type="linear",  # Set learning rate schedule to linear decay
-        warmup_steps=2,  # Set number of warmup steps for the scheduler
-    )
-
-    # Initialize Trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        tokenizer=tokenizer,
-        data_collator=default_data_collator
-    )
-
-    # Train 
-    trainer.train()    
+    # If you want more details about each GPU
+    if gpu_count > 0:
+        for i in range(gpu_count):
+            print(f"\nGPU {i}: {torch.cuda.get_device_name(i)}")
+            # Get memory info in bytes
+            memory_allocated = torch.cuda.memory_allocated(i)
+            memory_reserved = torch.cuda.memory_reserved(i)
+            print(f"Memory Allocated: {memory_allocated/1024**2:.2f}MB")
+            print(f"Memory Reserved: {memory_reserved/1024**2:.2f}MB")
     
-    # save final model
-    model_name = "meta-llama/Llama-3.2-1B-ft"
-    model.save_pretrained(os.path.join(project_path, "models", model_name))
-    tokenizer.save_pretrained(os.path.join(project_path, "models", model_name))
+    # define paths
+    project_path = os.getcwd()
+    data_path = os.path.join(project_path, '..', '..', "data/raw/Hospitalization-Summarization.json")
+    save_data_path = os.path.join(project_path, '..', '..', 'data/processed')
+    model_name = 'Llama-3.2-1B'
+    model_path = os.path.join(project_path, '..', '..', '..', model_name)
+    metrics_save_path = os.path.join(project_path, '..', '..', 'results/metrics')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    # clear device cache
+    torch.cuda.empty_cache()
+    # define args
+    args = argparse.Namespace()
+    args.device = device
+    args.batch_size = 1
+    args.max_training_epochs = 2
+    args.patience = 2
+    args.lr0 = 1e-3
+    args.lr_schedule = 'linear_decay'
+    args.lr_num_warmup_steps = 1
+    args.gradient_accumulation_steps = 4    
+    
+    # load model and tokenizer to device    
+    model, tokenizer = load_model_and_tokenizer(model_path)
+    model = get_tunable_model(model, args.device)
+    preprocess_data(data_path, save_data_path, max_samples=16, split_ratio=0.5)
+
+    
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=2, pin_memory=True
+    )
+    dev_dataloader = DataLoader(
+        val_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=2, pin_memory=True
+    )
+    test_dataloader = DataLoader(
+        test_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=2, pin_memory=True
+    )
+    args.num_training_steps = len(train_dataloader) * args.max_training_epochs
+
+
+    optimizer, lr_scheduler = define_optimizer(args, model)
+    
+    #print args
+    for i, arg in enumerate(args):
+        print(f"Args[{i}]: {arg}")
+        
+    # At the beginning of your script, create a dictionary to store metrics
+    metrics = {
+        'train_loss': [],
+        'train_perplexity': [],
+        'val_loss': [],
+        'val_perplexity': [],
+        'learning_rate': [],
+            'epoch': []
+            }
+
+    # logging with dictionary updates
+    # In the training loop:
+    model.train()
+    best_val_loss = float('inf')
+    patience = args.patience
+    n_steps = 0
+    trn_losses = []
+    print('begin training!')
+
+    for epoch in range(args.max_training_epochs):
+        print(f'epoch {epoch}/{args.max_training_epochs}')
+        with tqdm(total=len(train_dataloader)) as pbar:
+            for idx_b, batch in enumerate(train_dataloader):
+                n_steps += 1
+                
+                # forward pass 
+                batch = {k: v.to(args.device) for k, v in batch.items()}
+                outputs = model(**batch)
+                
+                # compute loss, gradient step 
+                loss = outputs.loss / args.gradient_accumulation_steps
+                loss.backward()
+                
+                if (n_steps % args.gradient_accumulation_steps == 0) or (n_steps == len(train_dataloader)):
+                    optimizer.step()
+                    optimizer.zero_grad()
+                
+                lr_scheduler.step()
+                
+                detached_loss = loss.detach().float()
+                trn_losses.append(detached_loss)
+                
+                # Store step-level metrics
+                metrics['train_loss'].append(float(detached_loss))
+                metrics['train_perplexity'].append(float(torch.exp(detached_loss)))
+                pbar.update(1)
+        
+        # Validation loop remains same but store metrics
+        val_losses = []
+        with tqdm(total=len(dev_dataloader)) as pbar:
+            for batch in dev_dataloader:
+                batch = {k: v.to(args.device) for k, v in batch.items()}
+                with torch.no_grad():
+                    outputs_val = model(**batch)
+                    val_losses.append(outputs_val.loss.detach().float())
+                pbar.update(1)
+
+        # Calculate epoch metrics
+        trn_loss_epoch = sum(trn_losses) / len(trn_losses)
+        val_loss_epoch = sum(val_losses) / len(val_losses)
+        trn_perplexity_epoch = torch.exp(trn_loss_epoch)
+        val_perplexity_epoch = torch.exp(val_loss_epoch)
+        
+        # Store epoch-level metrics
+        metrics['epoch'].append(epoch)
+        metrics['learning_rate'].append(float(lr_scheduler.get_lr()[0]))
+        metrics['val_loss'].append(float(val_loss_epoch))
+        metrics['val_perplexity'].append(float(val_perplexity_epoch))
+
+        print(f"epoch: {epoch}/{args.max_training_epochs}, "
+            f"trn_loss_epoch: {trn_loss_epoch}, "
+            f"trn_perplexity_epoch: {trn_perplexity_epoch}, "
+            f"val_loss_epoch: {val_loss_epoch}, "
+            f"val_perplexity_epoch: {val_perplexity_epoch}, "
+            f"lr: {lr_scheduler.get_lr()[0]}")
+
+        # Save metrics to JSON file after each epoch
+        model_epoch_metrics_path = os.path.join(metrics_save_path, f'{model_name}', f'{epoch}')
+        if not os.path.exists(model_epoch_metrics_path):
+            os.makedirs(model_epoch_metrics_path)
+        with open(os.path.join(model_epoch_metrics_path, 'training_metrics.json'), 'w') as f:
+            json.dump(metrics, f, indent=4)
+
+        # early stopping
+        if val_loss_epoch > best_val_loss:
+            if patience == 0:
+                print(f'stopping early at epoch {epoch}!')
+                break
+            else:
+                patience -= 1
+        else:
+            patience = args.patience
+            best_val_loss = val_loss_epoch
+        
+    # clear device cache
+    torch.cuda.empty_cache()
 
 if __name__ == '__main__':
     main()
